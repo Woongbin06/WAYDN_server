6626dd21da99c0702ad437a71371480d96cf3ff1
Woongbin06 <kob05314@gmail.com>
2022-11-17 22:57:41 +0900

    User Entity 생성

---
+++ src/main/java/com/WAYDN/waydn/condition/domain/condition/Condition.java
@@ -0,0 +1,31 @@
+package com.WAYDN.waydn.condition.domain.condition;
+
+import com.WAYDN.waydn.condition.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Condition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "condition_id")
+    private long id;
+
+    @Column(length = 20, nullable = false)
+    private String content;
+
+    @Builder
+    public Condition(String content) {
+        this.content = content;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/WAYDN/waydn/condition/domain/user/User.java
@@ -0,0 +1,43 @@
+package com.WAYDN.waydn.condition.domain.user;
+
+import com.WAYDN.waydn.condition.domain.condition.Condition;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    private int classNumber; // 학번
+
+    @Column(name = "username")
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String email, int classNumber, String name, String password) {
+        this.email = email;
+        this.classNumber = classNumber;
+        this.name = name;
+        this.password = password;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private final List<Condition> conditions = new ArrayList<>();
+}
