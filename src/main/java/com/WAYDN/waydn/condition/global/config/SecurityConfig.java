aa43a591870f6a718700d3d9b8874fcbe8508370
Woongbin06 <kob05314@gmail.com>
2023-01-10 23:02:35 +0900

    ADD : SecurityConfig 추가

--- src/main/java/com/WAYDN/waydn/condition/global/config/SecurityConfig.java
+++ src/main/java/com/WAYDN/waydn/condition/global/config/SecurityConfig.java
@@ -1,25 +1,28 @@
 package com.WAYDN.waydn.condition.global.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("user/join").permitAll()
+                .antMatchers("user/login").permitAll()
+                .antMatchers("/find/{name}").permitAll()
+                .antMatchers("/find").permitAll();
     }
 }
